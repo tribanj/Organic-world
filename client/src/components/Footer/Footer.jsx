f4d87d3963381abd0ba6b34c9b43a7605b1b2b70
Tribhuwan <tribhuwanja@gmail.com>
2023-08-25 16:35:36 +0530

    newsletter is complete

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,61 @@
 import "./Footer.scss";
+
+import React from "react";
+
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+
+import Payment from "../../assets/payments.png";
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum a
+            ipsam magnam alias voluptatibus assumenda architecto! Omnis eveniet,
+            ducimus earum sit deserunt atque consequuntur unde, quas, possimus
+            officiis dolores ipsa.
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-items">
+            <FaLocationArrow />
+            <div className="text">Ikgptu jalandhar kapurthala</div>
+          </div>
+          <div className="c-items">
+            <FaMobileAlt />
+            <div className="text">1234567890</div>
+          </div>
+          <div className="c-items">
+            <FaEnvelope />
+            <div className="text">Email:dummy@gmail.com</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+          <div className="text">Menu item-1</div>
+        </div>
+        <div className="col">
+          <div className="title">Pages</div>
+          <div className="text">Home</div>
+          <div className="text">About</div>
+          <div className="text">Privacy Policy</div>
+          <div className="text">Return</div>
+          <div className="text">Terms & condition</div>
+          <div className="text">Contact us</div>
+          <div className="text">Menu item-1</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -9,7 +9,35 @@ import {
 } from "react-icons/fa";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest updates and offers</span>
+        <div className="form">
+          <input type="text" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with our privacy Policy{" "}
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebook size={14} />
+          </div>
+          <div className="icon">
+            <FaInstagram size={14} />
+          </div>
+          <div className="icon">
+            <FaLinkedinIn size={14} />
+          </div>
+          <div className="icon">
+            <FaTwitter size={14} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
