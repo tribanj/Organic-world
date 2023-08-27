0b7bc151a640892ac7a26abfba341acf03f60722
Tribhuwan <tribhuwanja@gmail.com>
2023-08-27 11:49:21 +0530

    related product is complkete

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,42 @@
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+import CartItem from "./CartItem/CartItem";
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-button" onClick={() => setShowCart(false)}>
+            <span className="text">Close</span>
+            <MdClose />
+          </span>
+        </div>
+
+        {/* <div className="empty-cart">
+            <BsCartX />
+            <span>No products in the cart</span>
+            <button className="return-cta">Return to shop</button>
+        </div> */}
+
+        <>
+          <CartItem />
+          <div className="cart-footer">
+            <div className="subtotlal">
+                <span className="text">Subtotal : </span>
+                <span className="text total">₹1234</span>
+            </div>
+
+            <div className="button">
+                <button className="checkout-cta">Checkout</button>
+            </div>
+          </div>
+        </>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,6 +16,7 @@ import "./Header.scss";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -30,24 +31,27 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Category</li>
-        </ul>
-      </div>
-      <div className="center">ELECTRICWORLD</div>
-      <div className="right">
-        <TbSearch />
-        <AiOutlineHeart />
-        <span className="cart-icon">
-          <CgShoppingCart />
-          <span>5</span>
-        </span>
-      </div>
-    </header>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Category</li>
+          </ul>
+        </div>
+        <div className="center">ELECTRICWORLD</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon" onClick={() => setShowCart(true)}>
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+    </>
   );
 };
 
