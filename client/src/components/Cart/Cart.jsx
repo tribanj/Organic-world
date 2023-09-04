cff2aedb9fb358f2949873e1e31c12ed9b259cc9
Tribhuwan <tribhuwanja@gmail.com>
2023-09-04 16:40:12 +0530

    cart functionality added

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,42 +1,86 @@
-import "./Cart.scss";
-import CartItem from "./CartItem/CartItem";
+import React, { useContext } from "react";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
-const Cart = ({ setShowCart }) => {
-  return (
-    <div className="cart-panel">
-      <div className="opac-layer"></div>
-      <div className="cart-content">
-        <div className="cart-header">
-          <span className="heading">Shopping Cart</span>
-          <span className="close-button" onClick={() => setShowCart(false)}>
-            <span className="text">Close</span>
-            <MdClose />
-          </span>
+import { Context } from "../../utills/context";
+import CartItem from "./CartItem/CartItem";
+import { loadStripe } from "@stripe/stripe-js";
+// import { makePaymentRequest } from "../../utils/api";
+
+import "./Cart.scss";
+
+const Cart = () => {
+    const { cartItems, setShowCart, cartSubTotal } = useContext(Context);
+
+    const stripePromise = loadStripe(
+        process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+    );
+
+    // const handlePayment = async () => {
+    //     try {
+    //         const stripe = await stripePromise;
+    //         const res = await makePaymentRequest.post("/api/orders", {
+    //             products: cartItems,
+    //         });
+    //         await stripe.redirectToCheckout({
+    //             sessionId: res.data.stripeSession.id,
+    //         });
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
+
+    return (
+        <div className="cart-panel">
+            <div
+                className="opac-layer"
+                onClick={() => setShowCart(false)}
+            ></div>
+            <div className="cart-content">
+                <div className="cart-header">
+                    <span className="heading">Shopping Cart</span>
+                    <span
+                        className="close-btn"
+                        onClick={() => setShowCart(false)}
+                    >
+                        <MdClose className="close-btn" />
+                        <span className="text">close</span>
+                    </span>
+                </div>
+
+                {!cartItems.length && (
+                    <div className="empty-cart">
+                        <BsCartX />
+                        <span>No products in the cart.</span>
+                        <button className="return-cta" onClick={() => {}}>
+                            RETURN TO SHOP
+                        </button>
+                    </div>
+                )}
+
+                {!!cartItems.length && (
+                    <>
+                        <CartItem />
+                        <div className="cart-footer">
+                            <div className="subtotal">
+                                <span className="text">Subtotal:</span>
+                                <span className="text total">
+                                    &#8377;{cartSubTotal}
+                                </span>
+                            </div>
+                            <div className="button">
+                                <button
+                                    className="checkout-cta"
+                                    // onClick={handlePayment}
+                                >
+                                    Checkout
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
-
-        {/* <div className="empty-cart">
-            <BsCartX />
-            <span>No products in the cart</span>
-            <button className="return-cta">Return to shop</button>
-        </div> */}
-
-        <>
-          <CartItem />
-          <div className="cart-footer">
-            <div className="subtotal">
-                <span className="text">Subtotal : </span>
-                <span className="text total">₹1234</span>
-            </div>
-
-            <div className="button">
-                <button className="checkout-cta">Checkout</button>
-            </div>
-          </div>
-        </>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Cart;
--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,29 +1,51 @@
+import React, { useContext } from "react";
 import { MdClose } from "react-icons/md";
+import { Context } from "../../../utills/context";
+
 import "./CartItem.scss";
-import prod from "../../../assets/products/earbuds-prod-3.webp";
 const CartItem = () => {
+  const { cartItems, handleRemoveFromCart, handleCartProductQuantity } =
+    useContext(Context);
+
   return (
     <div className="cart-products">
-      <div className="cart-product">
-        <div className="img-container">
-          <img src={prod} alt="product" />
-        </div>
-        <div className="prod-details">
-          <div className="name">product name</div>
-          <MdClose className="close-btn" />
-          <div className="quantity-buttons">
-            <span>-</span>
-            <span>5</span>
-            <span>+</span>
+      {cartItems?.map((item) => (
+        <div className="search-result-item" key={item.id} onClick={() => {}}>
+          <div className="image-container">
+            <img
+              src={
+                process.env.REACT_APP_DEV_URL +
+                (item.attributes.img?.data[0]?.attributes?.url ?? "")
+              }
+              alt="cart-img"
+            />
           </div>
-
-          <div className="text">
-            <span>3</span>
-            <span>x</span>
-            <span className="highlight">₹1234</span>
+          <div className="prod-details">
+            <span className="name">{item.attributes.title}</span>
+            <MdClose
+              className="close-btn"
+              onClick={() => handleRemoveFromCart(item)}
+            />
+            <div className="quantity-buttons">
+              <span onClick={() => handleCartProductQuantity("dec", item)}>
+                -
+              </span>
+              <span>{item.attributes.quantity}</span>
+              <span onClick={() => handleCartProductQuantity("inc", item)}>
+                +
+              </span>
+            </div>
+            <div className="text">
+              <span>{item.attributes.quantity}</span>
+              <span>x</span>
+              <span className="highlight">
+                <span>&#8377;</span>
+                {item.attributes.price * item.attributes.quantity}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
